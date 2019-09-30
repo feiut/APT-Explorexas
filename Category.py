1c3cfc309fde93562396f3c783537f085aa38fd5
SONG <33387501+wsong1106@users.noreply.github.com>
2019-09-29 22:31:31 -0500

    Delete Category.py

--- Category.py
+++
@@ -1,8 +0,0 @@
-class Category:
-    def __init__(self, catName, cat_id = None):
-        self.catName = catName
-        self.cat_id = cat_id
-
-    def toQuery(self):
-        query = {"catName": self.catName}
-        return query
