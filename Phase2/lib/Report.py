2a01385a47814c1b2e7b543bb8a1393c79a7643b
Fei <feihe95@outlook.com>
2019-10-16 17:21:12 -0500

    auto generate binary reportID and imageID

--- Phase2/lib/Report.py
+++ Phase2/lib/Report.py
@@ -1,8 +1,19 @@
 class Report:
 
-    def __init__(self, reportId, userId, placeName, coordinates, categoryId, imgId, review, rating, timeStamp):
+    def __init__(self, 
+                reportId, 
+                userId, 
+                title,
+                placeName, 
+                coordinates, 
+                categoryId, 
+                imgId, 
+                review, 
+                rating, 
+                timeStamp):
         self.reportId = reportId
         self.userId = userId
+        self.title = title
         self.placeName = placeName
         self.coordinates = coordinates
         self.categoryId = categoryId
@@ -12,6 +23,16 @@ class Report:
         self.timeStamp = timeStamp
 
     def toQuery(self):
-        query = {"reportId": self.reportId, "userId": self.userId, "placeName": self.placeName, "coordinates": self.coordinates, "categoryId": self.categoryId,
-                 "imgId": self.imgId, "review": self.review, "rating": self.rating, "timeStamp": self.timeStamp}
+        query = {
+                "reportId": self.reportId, 
+                "userId": self.userId, 
+                "title": self.title, 
+                "placeName": self.placeName, 
+                "coordinates": self.coordinates, 
+                "categoryId": self.categoryId,
+                "imgId": self.imgId, 
+                "review": self.review, 
+                "rating": self.rating, 
+                "timeStamp": self.timeStamp
+                 }
         return query
