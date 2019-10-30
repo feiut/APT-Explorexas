154c4ba87d6ed3262bdac82439aff7bbabdb1d5c
[wsong1106] <[wangsong_1996@hotmail.com]>
2019-10-30 16:05:48 -0500

    take a photo

--- Phase3/app/src/main/java/com/example/phase3/MainActivity.kt
+++ Phase3/app/src/main/java/com/example/phase3/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.phase3
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.take_picture.*
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.take_picture)
+//        var btnCamera: Button = findViewById (R.id.btnCamera)
+//
+//        btnCamera.setOnClickListener(View.OnClickListener {
+//            fun onClick ( view : View ){
+//                var intent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//                startActivityForResult(intent, 0)
+//            }
+//        })
+    }
+
+    fun btnOnClick(view:View){
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, 0)
+    }
+
+    @Override
+    internal fun onActivityResult (requestCode : Int, resultCode : Int, data : Intent){
+        super.onActivityResult(requestCode  , resultCode , data)
+        val bitmap: Bitmap = data.extras?.get("data") as Bitmap
+        val imageView: ImageView = findViewById(R.id.imageView)
+        imageView.setImageBitmap(bitmap)
     }
 }
