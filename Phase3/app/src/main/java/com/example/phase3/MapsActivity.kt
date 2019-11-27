13373ba0bfd20c6ff3ef6023b513604c249058af
[wsong1106] <[wangsong_1996@hotmail.com]>
2019-11-27 15:35:21 -0600

    update map

--- Phase3/app/src/main/java/com/example/phase3/MapsActivity.kt
+++ Phase3/app/src/main/java/com/example/phase3/MapsActivity.kt
@@ -16,6 +16,7 @@ import android.location.Location
 import android.view.View
 import android.widget.ListView
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -24,9 +25,7 @@ import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-
+import com.google.android.gms.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -39,25 +38,31 @@ import kotlinx.android.synthetic.main.activity_category_report.*
 import kotlinx.android.synthetic.main.activity_view_categories.*
 import org.json.JSONArray
 import org.json.JSONObject
+import com.google.android.gms.location.*
+import android.Manifest
+import android.os.Looper
+import android.provider.Settings
+import android.widget.Toast
 
 import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    var PERMISSION_ID = 42
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
+    var thisLatitude = 37.7
+    var thisLongitude = -97.1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         setContentView(R.layout.activity_maps)
+        getLastLocation()
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this::onMapReady)
-
-
-//        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-//        getLastLocation()
     }
 
     /**
@@ -71,19 +76,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-        //Add a marker in Sydney and move the camera
-        val current = LatLng(30.355, -97.752)
-
-        mMap.addMarker(MarkerOptions().position(current).title("You are here!"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(current))
         getAllReports()
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current,mMap.maxZoomLevel*0.85.toFloat()))
     }
 
     fun getAllReports(){
         val queue = Volley.newRequestQueue(this)
-        val url = "http://apt-team7.appspot.com"
+        val url = "http://apt-team7.appspot.com/mobile"
         val stringRequest = StringRequest(
             Request.Method.GET, url,
             Response.Listener {
@@ -104,25 +102,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         queue.add(stringRequest)
     }
     fun bind(categories: JsonArray) {
-//        Log.d("debug", categories.toString())
+        Log.d("debug", categories.toString())
         for(category in categories){
             var categoryJson = JSONObject(category.toString()!!)
+            Log.d("debug", category.toString())
             var category_id = categoryJson.getString("cat_id")
             val queue = Volley.newRequestQueue(this)
-            val cat_url: String = CategoryReportActivity.webUrl +"viewCategoryPost/" + category_id
+            val cat_url: String = "http://apt-team7.appspot.com/viewCategoryPost/" + category_id
             val jsonArrayRequest = JsonArrayRequest(Request.Method.GET, cat_url, null,
                 Response.Listener<JSONArray> { response ->
-                    CategoryReportActivity.reportContentList = response
-                    for(reportIdx in 0 until CategoryReportActivity.reportContentList.length()) {
-                        val report: JSONObject = CategoryReportActivity.reportContentList.getJSONObject(reportIdx)
+                    var reportContentList = response
+                    for (reportIdx in 0 until reportContentList.length()) {
+                        val report: JSONObject = reportContentList.getJSONObject(reportIdx)
+                        Log.d("debug", report.toString())
                         var coordinate = report.getString("coordinates")
-                        var latitude = coordinate.split(',')[0].subSequence(2,10).toString().toDouble()
-                        var longitude = coordinate.split(',')[1].subSequence(1,10).toString().toDouble()
+                        var latitude =
+                            coordinate.split(',')[0].subSequence(2, 10).toString().toDouble()
+                        var longitude =
+                            coordinate.split(',')[1].subSequence(1, 10).toString().toDouble()
                         val current = LatLng(latitude, longitude)
-                        mMap.addMarker(MarkerOptions()
-                            .position(current)
-                            .title("Report: " + report.getString("title"))
-                            .snippet("Description: "+ report.getString("review")))
+                        mMap.addMarker(
+                            MarkerOptions()
+                                .position(current)
+                                .title("Report: " + report.getString("title"))
+                                .snippet("Description: " + report.getString("review"))
+                        )
                     }
                 },
                 Response.ErrorListener { error->
@@ -132,59 +136,88 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
 
     }
-//    @SuppressLint("MissingPermission")
-//    private fun getLastLocation() {
-//        fusedLocationClient.lastLocation.addOnSuccessListener {
-//            it ?: return@addOnSuccessListener
-//            handleLocation(it)
-//        }
-//    }
-//
-//    private fun handleLocation(location: Location) {
-//        val geocoder = Geocoder(this, Locale.getDefault())
-//        Thread(Runnable {
-//            try {
-//                var addresses = geocoder.getFromLocation(
-//                    location.latitude, location.longitude, 1
-//                )
-//                var current = LatLng(location.latitude, location.longitude)
-//                mMap.addMarker(MarkerOptions().position(current).title("You are here"))
-//                mMap.moveCamera(CameraUpdateFactory.newLatLng(current))
-//                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current,mMap.maxZoomLevel*2/3))
-//                Log.i("TAG", addresses.first().toString())
-//            } catch (e: Exception) {
-//                e.printStackTrace()
-//            }
-//        }).start()
-//    }
-//
-//    fun Context.hasPermissions(vararg permission: String): Boolean {
-//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return true
-//        return permission.all {
-//            ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
-//        }
-//    }
-//    fun Activity.requestPermissions(vararg permissions: String, requestCode: Int) {
-//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return
-//        this.requestPermissions(permissions, requestCode)
-//    }
-//    fun Activity.shouldShowCustomPermissionRequestHint(permission: String): Boolean {
-//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return false
-//        return !shouldShowRequestPermissionRationale(permission)
-//    }
-//    fun Context.LocationServiceEnable(): Boolean {
-//        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-//        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
-//                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-//    }
-//    fun Context.appSettingIntent(): Intent {
-//        return Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-//            data = Uri.parse("package:$packageName")
-//        }
-//    }
-//    fun LocationSettingIntent(): Intent {
-//        return Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-//    }
 
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ), PERMISSION_ID
+        )
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == PERMISSION_ID) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                // Granted. Start getting the location information
+            }
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    var location: Location? = task.result
+                    if (location == null) {
+                        requestNewLocationData()
+                    } else {
+                        var current = LatLng(location.latitude, location.longitude)
+                        mMap.addMarker(MarkerOptions().position(current).title("You are here!"))
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(current))
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current,(mMap.minZoomLevel+mMap.maxZoomLevel)*3/4))
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+        var mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mFusedLocationClient!!.requestLocationUpdates(
+            mLocationRequest, mLocationCallback,
+            Looper.myLooper()
+        )
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            var mLastLocation: Location = locationResult.lastLocation
+            Log.d("debug1", mLastLocation.latitude.toString())
+            Log.d("debug1", mLastLocation.longitude.toString())
+            thisLatitude = mLastLocation.latitude
+            thisLongitude = mLastLocation.longitude
+        }
+    }
 
 }
